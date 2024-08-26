1283d247c866d3bf4bb5cdf5184ffa3cd2d911d8
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-08-26 12:15:05 -0500

    feat: :sparkles: Se realiza ajustes de htm y css teniendo en cuenta el paso a web components
    Se realiza la configuracion para trabajar basandonos en web components

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const routes= require('./server/routes/route')
-app.use(express.json());
+const routes = require('./server/routes/route');
 
+// Configuración del puerto y host
 const config = {
     port: process.env.EXPRESS_PORT || 3000,
     host: process.env.EXPRESS_HOST || 'localhost',
-    static: process.env.EXPRESS_STATIC
+    static: process.env.EXPRESS_STATIC || 'public'
 };
 
+// Middleware para servir archivos estáticos
 app.use(express.static(config.static));
 
+// Ruta principal para servir el archivo HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(config.static, 'index.html'));
 });
 
+// Rutas adicionales
+app.use('/api', routes);
+
+// Middleware para manejar errores
 app.use((err, req, res, next) => {
     res.status(err.status || 500).json({
         status: err.status || 500,
@@ -23,6 +29,7 @@ app.use((err, req, res, next) => {
     });
 });
 
+// Iniciar el servidor
 app.listen(config.port, config.host, () => {
     console.log(`http://${config.host}:${config.port}`);
 });
